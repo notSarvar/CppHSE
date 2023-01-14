8d754e40b5ee0a2636c0f3608982c32945f51b08
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-14 03:12:24 +0300

    Initial commit

--- tasks/multiplication/multiplication.cpp
+++ tasks/multiplication/multiplication.cpp
@@ -1,6 +1,5 @@
 #include "multiplication.h"
 
 int64_t Multiply(int a, int b) {
-    int64_t ans = static_cast<int64_t>(a) * static_cast<int64_t>(b);
-    return ans;
+    return static_cast<int64_t>(a) * static_cast<int64_t>(b);
 }
