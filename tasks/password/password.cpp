1a2d61ba27b0daa8a50d0b73b86ea78c5b5ca060
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-20 23:03:33 +0300

    Rewrote by codestyle

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,10 +1,10 @@
 #include "password.h"
 
 bool ValidatePassword(const std::string& password) {
-    bool ans_flag = true ;
+    bool ans_flag = true;
 
     if (password.size() < 8 or password.size() > 14) {
-        ans_flag = false ;
+        ans_flag = false;
     }
 
     int cnt_special[5] = {0};
@@ -13,27 +13,27 @@ bool ValidatePassword(const std::string& password) {
         int ascii_code = static_cast<int>(t);
 
         if (ascii_code < 33 or ascii_code > 126) {
-            ans_flag = false ;
-            break ;
+            ans_flag = false;
+            break;
         }
 
         switch (ascii_code) {
             case 97 ... 122:
                 cnt_special[0] = 1;
-                break ;
+                break;
             case 65 ... 90:
                 cnt_special[1] = 1;
-                break ;
+                break;
             case 48 ... 57:
                 cnt_special[2] = 1;
-                break ;
+                break;
             default:
                 cnt_special[3] = 1;
         }
     }
 
     if (std::count(cnt_special, cnt_special + 4, 1) < 3) {
-        ans_flag = false ;
+        ans_flag = false;
     }
 
     return ans_flag;
