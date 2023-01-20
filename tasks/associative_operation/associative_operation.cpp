834d508cfb313bccc28344bcadb977fcd38f598c
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 01:29:09 +0300

    Fixed extra changes and other mistakes

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -3,7 +3,7 @@
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
     for (size_t i = 0; i < table.size(); ++i) {
         for (size_t j = 0; j < table[i].size(); ++j) {
-            if (table[j][table[i][j]] != i or table[table[j][i]][i] != j) {
+            if (table[j][table[i][j]] != i or table[table[i][j]][i] != j) {
                 return false;
             }
         }
--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,5 +1,4 @@
 #include "sort_students.h"
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
-
 }
