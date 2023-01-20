164378451df2f88b03479774e31837c55c117d32
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 02:33:37 +0300

    Found a solution

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -2,9 +2,11 @@
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
     for (size_t i = 0; i < table.size(); ++i) {
-        for (size_t j = 0; j < table[i].size(); ++j) {
-            if (table[j][table[i][j]] != i or table[table[i][j]][i] != j) {
-                return false;
+        for (size_t j = 0; j < table.size(); ++j) {
+            for (size_t k = 0; k < table.size(); ++k) {
+                if (table[table[i][j]][k] != table[i][table[j][k]]) {
+                    return false;
+                }
             }
         }
     }
