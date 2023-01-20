d4ac1d29c9a254f5ab3c8ef361f695e719db05bd
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 01:12:54 +0300

    The first try

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -1,5 +1,12 @@
 #include "associative_operation.h"
 
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
-    return false;
+    for (size_t i = 0; i < table.size(); ++i) {
+        for (size_t j = 0; j < table[i].size(); ++j) {
+            if (table[j][table[i][j]] != i) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
