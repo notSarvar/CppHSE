ff216ddd8ee3e0a914ead2827dbe9eedd9e2cccc
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-20 23:16:25 +0300

    Rewrote with defines

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -3,6 +3,14 @@
 #define min_length 8
 #define max_length 14
 #define cnt_of_specials 4
+#define min_ascii 33
+#define max_ascii 126
+#define a_ascii 97
+#define z_ascii 122
+#define A_ascii 65
+#define Z_ascii 90
+#define zero_ascii 48
+#define nine_ascii 57
 
 bool ValidatePassword(const std::string& password) {
     bool ans_flag = true;
@@ -16,19 +24,19 @@ bool ValidatePassword(const std::string& password) {
     for (char t : password) {
         int ascii_code = static_cast<int>(t);
 
-        if (ascii_code < 33 or ascii_code > 126) {
+        if (ascii_code < min_ascii or ascii_code > max_ascii) {
             ans_flag = false;
             break;
         }
 
         switch (ascii_code) {
-            case 97 ... 122:
+            case a_ascii ... z_ascii:
                 cnt_special[0] = 1;
                 break;
-            case 65 ... 90:
+            case A_ascii ... Z_ascii:
                 cnt_special[1] = 1;
                 break;
-            case 48 ... 57:
+            case zero_ascii ... nine_ascii:
                 cnt_special[2] = 1;
                 break;
             default:
