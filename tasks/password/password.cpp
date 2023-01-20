39615da4565a151afd2c9bea75a1fe272cce8e4b
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-20 23:11:25 +0300

    Trying defines

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,13 +1,17 @@
 #include "password.h"
 
+#define min_length 8
+#define max_length 14
+#define cnt_of_specials 4
+
 bool ValidatePassword(const std::string& password) {
     bool ans_flag = true;
 
-    if (password.size() < 8 or password.size() > 14) {
+    if (password.size() < min_length or password.size() > max_length) {
         ans_flag = false;
     }
 
-    int cnt_special[5] = {0};
+    int cnt_special[cnt_of_specials] = {0};
 
     for (char t : password) {
         int ascii_code = static_cast<int>(t);
