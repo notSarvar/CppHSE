9b714c2ae81fc9c4536dd0e98da545f4a836bd8e
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 00:31:07 +0300

    First try

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,4 +1,21 @@
 #include "sort_students.h"
 
+#define bd birth_date
+
+bool comp_date(Student& a, Student& b) {
+    return std::tie(a.bd.year, a.bd.month, a.bd.day, a.last_name, a.name) <
+    std::tie(b.bd.year, b.bd.month, b.bd.day, b.last_name, b.name);
+}
+
+bool comp_name(Student& a, Student& b) {
+    return std::tie(a.last_name, a.name, a.bd.year, a.bd.month, a.bd.day) <
+           std::tie(b.last_name, b.name, b.bd.year, b.bd.month, b.bd.day);
+}
+
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
+    if(sortKind == SortKind::Date) {
+        std::sort(students.begin(), students.end(), comp_date);
+    } else {
+        std::sort(students.begin(), students.end(), comp_name);
+    }
 }
