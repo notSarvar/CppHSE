8e7dfeaa5be0c960ecc8593be8475684844c4209
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 00:38:56 +0300

    Fixed codestyle

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -3,13 +3,13 @@
 #define bd birth_date
 
 bool comp_date(Student& a, Student& b) {
-    return std::tie(a.bd.year, a.bd.month, a.bd.day, a.last_name, a.name) <
-           std::tie(b.bd.year, b.bd.month, b.bd.day, b.last_name, b.name);
+    return tie(a.bd.year, a.bd.month, a.bd.day, a.last_name, a.name) <
+           tie(b.bd.year, b.bd.month, b.bd.day, b.last_name, b.name);
 }
 
 bool comp_name(Student& a, Student& b) {
-    return std::tie(a.last_name, a.name, a.bd.year, a.bd.month, a.bd.day) <
-           std::tie(b.last_name, b.name, b.bd.year, b.bd.month, b.bd.day);
+    return tie(a.last_name, a.name, a.bd.year, a.bd.month, a.bd.day) <
+           tie(b.last_name, b.name, b.bd.year, b.bd.month, b.bd.day);
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
