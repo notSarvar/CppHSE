62bfa30448015cf93d46e6ae9371fab9385f1088
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 01:45:40 +0300

    Fixed remarks

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,22 +1,16 @@
 #include "sort_students.h"
 #include <tuple>
 
-#define bd birth_date
-
-bool comp_date(Student& a, Student& b) {
-    return std::tie(a.bd.year, a.bd.month, a.bd.day, a.last_name, a.name) <
-           std::tie(b.bd.year, b.bd.month, b.bd.day, b.last_name, b.name);
+bool by_date(Student& a, Student& b) {
+    return std::tie(a.birth_date.year, a.birth_date.month, a.birth_date.day, a.last_name, a.name) <
+           std::tie(b.birth_date.year, b.birth_date.month, b.birth_date.day, b.last_name, b.name);
 }
 
-bool comp_name(Student& a, Student& b) {
-    return std::tie(a.last_name, a.name, a.bd.year, a.bd.month, a.bd.day) <
-           std::tie(b.last_name, b.name, b.bd.year, b.bd.month, b.bd.day);
+bool by_name(Student& a, Student& b) {
+    return std::tie(a.last_name, a.name, a.birth_date.year, a.birth_date.month, a.birth_date.day) <
+           std::tie(b.last_name, b.name, b.birth_date.year, b.birth_date.month, b.birth_date.day);
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
-    if(sortKind == SortKind::Date) {
-        std::sort(students.begin(), students.end(), comp_date);
-    } else {
-        std::sort(students.begin(), students.end(), comp_name);
-    }
+    std::sort(students.begin(), students.end(), (sortKind == SortKind::Date) ? by_date: by_name);
 }
