28025254b6c526a71dbb00c551ac3771377fa71b
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 01:25:28 +0300

    Fixed mistakes

--- tasks/associative_operation/associative_operation.cpp
+++ tasks/associative_operation/associative_operation.cpp
@@ -3,7 +3,7 @@
 bool IsAssociative(const std::vector<std::vector<size_t>>& table) {
     for (size_t i = 0; i < table.size(); ++i) {
         for (size_t j = 0; j < table[i].size(); ++j) {
-            if (table[j][table[i][j]] != i) {
+            if (table[j][table[i][j]] != i or table[table[j][i]][i] != j) {
                 return false;
             }
         }
