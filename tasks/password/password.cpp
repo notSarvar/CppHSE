9e9fa7530913c90ab7092232a6ea37a7770168dc
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 03:10:45 +0300

    Changed constants names

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,23 +1,23 @@
 #include "password.h"
 
-constexpr size_t MIN_LENGTH = 8;
-constexpr size_t CLASSES_CNT = 4;
-constexpr size_t MAX_LENGTH = 14;
-constexpr size_t MIN_CODE_REQUIRED = 33;
-constexpr size_t MAX_CODE_REQUIRED = 126;
+constexpr size_t MinLength = 8;
+constexpr size_t ClassesCnt = 4;
+constexpr size_t MaxLength = 14;
+constexpr size_t MinCodeRequired = 33;
+constexpr size_t MaxCodeRequired = 126;
 
 bool ValidatePassword(const std::string& password) {
 
-    if (password.size() < MIN_LENGTH || password.size() > MAX_LENGTH) {
+    if (password.size() < MinLength || password.size() > MaxLength) {
         return false;
     }
 
-    int cnt_special[CLASSES_CNT] = {0};
+    int cnt_special[ClassesCnt] = {0};
 
     for (char t : password) {
         int ascii_code = static_cast<int>(t);
 
-        if (ascii_code < MIN_CODE_REQUIRED || ascii_code > MAX_CODE_REQUIRED) {
+        if (ascii_code < MinCodeRequired || ascii_code > MaxCodeRequired) {
             return false;
         }
 
