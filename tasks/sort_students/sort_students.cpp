0b0e1caf59b373b0e5685a9b51f46d33f6d68256
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 22:41:11 +0300

    Fixed codestyle troubles

--- tasks/sort_students/sort_students.cpp
+++ tasks/sort_students/sort_students.cpp
@@ -1,16 +1,16 @@
 #include "sort_students.h"
 #include <tuple>
 
-bool by_date(Student& a, Student& b) {
+bool ByDate(Student& a, Student& b) {
     return std::tie(a.birth_date.year, a.birth_date.month, a.birth_date.day, a.last_name, a.name) <
            std::tie(b.birth_date.year, b.birth_date.month, b.birth_date.day, b.last_name, b.name);
 }
 
-bool by_name(Student& a, Student& b) {
+bool ByName(Student& a, Student& b) {
     return std::tie(a.last_name, a.name, a.birth_date.year, a.birth_date.month, a.birth_date.day) <
            std::tie(b.last_name, b.name, b.birth_date.year, b.birth_date.month, b.birth_date.day);
 }
 
 void SortStudents(std::vector<Student>& students, SortKind sortKind) {
-    std::sort(students.begin(), students.end(), (sortKind == SortKind::Date) ? by_date: by_name);
+    std::sort(students.begin(), students.end(), (sortKind == SortKind::Date) ? ByDate: ByName);
 }
