3bfb7e7696aaf37c67b58130f6bacac59811992b
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 15:45:14 +0300

    Fixed some mistakes

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -2,6 +2,7 @@
 
 constexpr size_t MinLength = 8;
 constexpr size_t ClassesCnt = 4;
+constexpr size_t ClassesRequired = 3;
 constexpr size_t MaxLength = 14;
 constexpr size_t MinCodeRequired = 33;
 constexpr size_t MaxCodeRequired = 126;
@@ -15,22 +16,21 @@ bool ValidatePassword(const std::string& password) {
     int cnt_special[ClassesCnt] = {0};
 
     for (char t : password) {
-        int ascii_code = static_cast<int>(t);
 
-        if (ascii_code < MinCodeRequired || ascii_code > MaxCodeRequired) {
+        if (t < MinCodeRequired || t > MaxCodeRequired) {
             return false;
         }
 
-        if (islower(ascii_code)) {
+        if (islower(t)) {
             cnt_special[0] = 1;
-        } else if (isupper(ascii_code)) {
+        } else if (isupper(t)) {
             cnt_special[1] = 1;
-        } else if (isdigit(ascii_code)) {
+        } else if (isdigit(t)) {
             cnt_special[2] = 1;
         } else {
             cnt_special[3] = 1;
         }
     }
 
-    return std::count(cnt_special, cnt_special + 4, 1) > 2;
+    return std::count(cnt_special, cnt_special + 4, 1) >= ClassesRequired;
 }
