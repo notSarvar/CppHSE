f3189b98ba09eced119b7c4770bf9500c6bc4879
Sarvar Yuldashev <sarvar010104@gmail.com>
2023-01-21 16:24:30 +0300

    FIxed some mistakes

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,5 +1,18 @@
 #include "palindrome.h"
 
 bool IsPalindrome(const std::string& str) {
-    return false;
+    std::string spaceless;
+    spaceless.reserve(str.size());
+    for (char t : str) {
+        if (t != ' ') {
+            spaceless += t;
+        }
+    }
+    const size_t length = spaceless.size();
+    for (size_t i = 0; i < length / 2; i++) {
+        if (spaceless[i] != spaceless[length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 }
