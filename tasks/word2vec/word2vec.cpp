70a946c5392841a5f393e95f50f02608cce95ce0
Sarvar <sarvar010104@gmail.com>
2023-02-10 23:59:18 +0300

    First try

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -1,8 +1,27 @@
 #include "word2vec.h"
 
 #include <vector>
+#include <unordered_map>
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    return {};
+    std::unordered_map<std::string, int> how_close;
+    int max_dist = INT_MIN;
+    for (int i = 1; i < vectors.size(); ++i) {
+        int sum = 0;
+        for (int j = 0; j < vectors[i].size(); ++j) {
+            sum += vectors[0][j] * vectors[i][j];
+        }
+        how_close[words[i]] = sum;
+        max_dist = std::max(max_dist, sum);
+    }
+    std::vector<std::string> closest_words;
+    for (const auto& i: words) {
+        if (how_close[i] == max_dist) {
+            closest_words.push_back(i);
+        }
+    }
+
+    return closest_words;
 }
+//Ya znayu eto ne proidet
