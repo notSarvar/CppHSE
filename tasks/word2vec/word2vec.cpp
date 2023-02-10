741939872f64f6a2228c2758fd5872e75d305c76
Sarvar <sarvar010104@gmail.com>
2023-02-11 00:04:12 +0300

    First try 2.0

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -3,10 +3,12 @@
 #include <vector>
 #include <unordered_map>
 
+const int INF = 2e9;
+
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
     std::unordered_map<std::string, int> how_close;
-    int max_dist = INT_MIN;
+    int max_dist = -INF;
     for (int i = 1; i < vectors.size(); ++i) {
         int sum = 0;
         for (int j = 0; j < vectors[i].size(); ++j) {
