29303db973f5226d4b7d2d5f6979c2e971fd321c
Sarvar <sarvar010104@gmail.com>
2023-02-11 02:36:42 +0300

    First try

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,6 +1,20 @@
 #include "tests_checking.h"
 
+#include <deque>
+
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    return {};
+    std::deque<std::string> checking_line;
+    for (const auto& i : student_actions) {
+        if (i.side == Side::Top) {
+            checking_line.push_front(i.name);
+        } else {
+            checking_line.push_back(i.name);
+        }
+    }
+    std::vector<std::string> students_order;
+    for (const auto& i : queries) {
+        students_order.push_back(checking_line[i - 1]);
+    }
+    return students_order;
 }
