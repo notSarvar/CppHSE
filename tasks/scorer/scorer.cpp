4ff376a70e7f196f1ffcbfe52d70c402d7f01b22
Sarvar <sarvar010104@gmail.com>
2023-02-11 21:59:45 +0300

    Second try 2.0

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -9,23 +9,33 @@ bool ByTime(const Event* a, const Event* b) {
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     std::vector<const Event*> sorted_events;
     for (const auto& i : events) {
-        sorted_events.push_back(&i);
+        sorted_events.push_back(&i);// добавить сюда std::move?
     }
     std::sort(sorted_events.begin(), sorted_events.end(), ByTime);
 
-    std::unordered_map<StudentName, std::unordered_map<TaskName, EventType>> all_events;
+    std::unordered_map<StudentName, std::unordered_map<TaskName, std::pair<int, int>>> all_events;
     for (const auto& i : sorted_events) {
         if (i->time > score_time) {
             break;
         }
-        all_events[i->student_name][i->task_name] = i->event_type;
+        all_events[i->student_name][i->task_name] = {0, 1};
+        if (i->event_type == EventType::CheckSuccess) {
+            all_events[i->student_name][i->task_name] = {1, all_events[i->student_name][i->task_name].second};
+        } else if (i->event_type == EventType::CheckFailed) {
+            all_events[i->student_name][i->task_name] = {0, all_events[i->student_name][i->task_name].second};
+        }
+        if (i->event_type == EventType::MergeRequestClosed) {
+            all_events[i->student_name][i->task_name] = {all_events[i->student_name][i->task_name].first, 1};
+        } else if (i->event_type == EventType::MergeRequestOpen) {
+            all_events[i->student_name][i->task_name] = {all_events[i->student_name][i->task_name].first, 0};
+        }
     }
 
     ScoreTable score_table;
     for (const auto& i : all_events) {
         for (const auto& j : i.second) {
-            if (j.second == EventType::CheckSuccess || j.second == EventType::MergeRequestClosed) {
-                score_table[i.first].insert(j.first);
+            if (j.second == std::make_pair(1, 1)) {
+                score_table[i.first].insert(j.first); // тут возникала ошибка с i.first когда поменял StudentName
             }
         }
     }
