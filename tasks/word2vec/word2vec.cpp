b4b31a69bb6983df096dbfb5f59892de967109b1
Sarvar <sarvar010104@gmail.com>
2023-02-12 20:17:44 +0300

    added changes

--- tasks/word2vec/word2vec.cpp
+++ tasks/word2vec/word2vec.cpp
@@ -7,7 +7,7 @@ const int64_t INF = 2e9;
 
 std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
                                           const std::vector<std::vector<int>>& vectors) {
-    std::unordered_map<std::string_view, int64_t> how_close;
+    std::unordered_map<std::string, int64_t> how_close;
     int64_t max_dist = -INF;
     for (size_t i = 1; i < vectors.size(); ++i) {
         int64_t sum = 0;
@@ -17,12 +17,11 @@ std::vector<std::string> FindClosestWords(const std::vector<std::string>& words,
         how_close[words[i]] = sum;
         max_dist = std::max(max_dist, sum);
     }
-    std::vector<std::string_view> closest_words;
+    std::vector<std::string> closest_words;
     for (const auto& i : words) {
         if (how_close[i] == max_dist) {
             closest_words.push_back(i);
         }
     }
-
-    return {closest_words.begin(), closest_words.end()};
+    return closest_words;
 }
