f314a84c2ab7f095e761e185caef1249d0be2400
Sarvar <sarvar010104@gmail.com>
2023-02-12 21:30:20 +0300

    fixed

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -7,30 +7,32 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
     }
 
     std::string cur_dir = static_cast<std::string>(current_working_dir);
-    int64_t prev_slash = -1;
+    size_t prev_slash = 0;
     if (path[0] == '/') {
         cur_dir = "/";
-        prev_slash = 0;
     }
-
     std::string_view cur_move;
     for (size_t i = 1; i < path.size(); ++i) {
         while (path[i] != '/' && i < path.size()) {
             ++i;
         }
-        cur_move = path.substr(prev_slash + 1, i - prev_slash - 1);
+        if (path[0] != '/' && prev_slash == 0) {
+            cur_move = path.substr(0, i);
+        } else {
+            cur_move = path.substr(prev_slash + 1, i - prev_slash - 1);
+        }
         if (cur_move.empty() || cur_move == ".") {
-            prev_slash = static_cast<int64_t>(i);
+            prev_slash = i;
             continue;
         } else if (cur_move == "..") {
-            prev_slash = static_cast<int64_t>(i);
+            prev_slash = i;
             if (cur_dir == "/") {
                 continue;
             }
             size_t last_slash = cur_dir.rfind('/', cur_dir.size() - 2);
             cur_dir.erase(last_slash + 1);
         } else {
-            prev_slash = static_cast<int64_t>(i);
+            prev_slash = i;
             if (cur_dir.back() != '/') {
                 cur_dir += '/';
             }
