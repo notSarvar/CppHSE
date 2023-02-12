da8c96a6ce43f7841d24eee6f4b36bc75d4aecdd
Sarvar <sarvar010104@gmail.com>
2023-02-12 20:46:02 +0300

    Fixed

--- tasks/unixpath/unixpath.cpp
+++ tasks/unixpath/unixpath.cpp
@@ -1,6 +1,6 @@
 #include "unixpath.h"
 
-// #include <iostream>
+#include <iostream>
 
 std::string NormalizePath(std::string_view current_working_dir, std::string_view path) {
 
@@ -9,42 +9,38 @@ std::string NormalizePath(std::string_view current_working_dir, std::string_view
     }
 
     std::string cur_dir = static_cast<std::string>(current_working_dir);
-    int64_t prev_slash = -1;
-    if (path[0] == '/') {
-        cur_dir = "/";
-        prev_slash = 0;
-    }
-
+    size_t prev_slash = 0;
     std::string_view cur_move;
     for (size_t i = 1; i < path.size(); ++i) {
         while (path[i] != '/' && i < path.size()) {
             ++i;
         }
-        cur_move = path.substr(prev_slash + 1, i - prev_slash - 1);
+        if (prev_slash == 0 && path[0] != '/') {
+            cur_move = path.substr(0, i);
+        } else {
+            cur_move = path.substr(prev_slash + 1, i - prev_slash - 1);
+        }
         if (cur_move.empty() || cur_move == ".") {
-            prev_slash = static_cast<int64_t>(i);
+            prev_slash = i;
             continue;
         } else if (cur_move == "..") {
-            prev_slash = static_cast<int64_t>(i);
+            prev_slash = i;
             if (cur_dir == "/") {
                 continue;
             }
             size_t last_slash = cur_dir.rfind('/', cur_dir.size() - 2);
-            //            std::cout<<last_slash <<"\n";
-            cur_dir = cur_dir.substr(0, last_slash + 1);
+            cur_dir.erase(last_slash + 1);
         } else {
-            prev_slash = static_cast<int64_t>(i);
-            if (cur_dir[cur_dir.size() - 1] != '/') {
+            prev_slash = i;
+            if (cur_dir.back() != '/') {
                 cur_dir += '/';
             }
             cur_dir += cur_move;
             cur_dir += "/";
         }
-        //         std::cout << cur_dir << "\n";
     }
-    if (cur_dir[cur_dir.size() - 1] == '/' && cur_dir.size() > 1) {
-        cur_dir.erase(cur_dir.size() - 1);
+    if (cur_dir.back() == '/' && cur_dir.size() > 1) {
+        cur_dir.pop_back();
     }
-    // std::cout << "---------\n";
     return cur_dir;
 }
