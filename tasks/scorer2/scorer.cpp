0ad41bd7c61aa55aee0b823552be91fc5de815fc
Sarvar <sarvar010104@gmail.com>
2023-02-25 21:41:58 +0300

    Fixed stupid mistake

--- tasks/scorer2/scorer.cpp
+++ tasks/scorer2/scorer.cpp
@@ -17,16 +17,16 @@ void Scorer::OnCheckFailed(const StudentName &student_name, const TaskName &task
 
 void Scorer::OnMergeRequestOpen(const StudentName &student_name, const TaskName &task_name) {
     this->all_actions_[student_name].merge_stat_ = false;
-    if (this->all_actions_[student_name].check_stat_ && this->all_actions_[student_name].merge_stat_) {
-        this->actual_scores_[student_name].insert(task_name);
+    this->actual_scores_[student_name].erase(task_name);
+    if (this->actual_scores_[student_name].empty()) {
+        this->actual_scores_.erase(student_name);
     }
 }
 
 void Scorer::OnMergeRequestClosed(const StudentName &student_name, const TaskName &task_name) {
     this->all_actions_[student_name].merge_stat_ = true;
-    this->actual_scores_[student_name].erase(task_name);
-    if (this->actual_scores_[student_name].empty()) {
-        this->actual_scores_.erase(student_name);
+    if (this->all_actions_[student_name].check_stat_ && this->all_actions_[student_name].merge_stat_) {
+        this->actual_scores_[student_name].insert(task_name);
     }
 }
 
