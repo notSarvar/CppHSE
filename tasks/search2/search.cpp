8a051577d904cdf4c379c6ee68532e290ac060ca
Sarvar <sarvar010104@gmail.com>
2023-02-25 22:34:45 +0300

    first changes

--- tasks/search2/search.cpp
+++ tasks/search2/search.cpp
@@ -1,1 +1,9 @@
 #include "search.h"
+
+void SearchEngine::BuildIndex(std::string_view text) {
+
+}
+
+std::vector<std::string_view> SearchEngine::Search(std::string_view query, size_t results_count) const {
+
+}
