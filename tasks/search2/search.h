b6623f0fa887c70deba9b64aeafb4890e60c0e2b
Sarvar <sarvar010104@gmail.com>
2023-02-26 00:24:26 +0300

    first changes 2.0

--- tasks/search2/search.h
+++ tasks/search2/search.h
@@ -1,12 +1,18 @@
 #pragma once
 
+#include <algorithm>
+#include <ctype.h>
+#include <cmath>
+#include <set>
+#include <string>
 #include <string_view>
 #include <vector>
+#include <unordered_map>
 
 class SearchEngine {
 public:
     void BuildIndex(std::string_view text);
     std::vector<std::string_view> Search(std::string_view query, size_t results_count) const;
 private:
-    std::string_view text_;
+    std::vector<std::vector<std::string_view>> text_;
 };
