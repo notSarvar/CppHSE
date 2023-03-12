79e1bf1213ce0ec9c1d3a9ffea626e97a0d2bd42
Sarvar <sarvar010104@gmail.com>
2023-03-13 00:08:40 +0300

    im tired

--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -14,7 +14,7 @@ class Poly {
 public:
     Poly();
     explicit Poly(std::vector<int64_t> coefs);
-    Poly(std::initializer_list<std::pair<const int64_t, int64_t>> coefs);
+    explicit Poly(std::initializer_list<std::pair<const int64_t, int64_t>> coefs);
     bool operator==(const Poly& other) const;
     Poly operator-() const;
     int64_t operator()(int64_t a) const;
