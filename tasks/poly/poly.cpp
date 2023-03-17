f05485e543238e96e8f8c4209e00bc9665c04133
Sarvar <sarvar010104@gmail.com>
2023-03-17 14:37:11 +0300

    Resolved all threads

--- tasks/poly/poly.cpp
+++ tasks/poly/poly.cpp
@@ -3,7 +3,16 @@
 #include <iostream>
 #include <cmath>
 
-Poly::Poly() = default;
+Poly& Poly::DeleteZeros(Poly& p) {
+    for (auto i = p.coefs_.begin(); i != p.coefs_.end();) {
+        if (i->second) {
+            i++;
+        } else {
+            i = p.coefs_.erase(i);
+        }
+    }
+    return p;
+}
 
 Poly::Poly(std::vector<int64_t> coefs) {
     for (int64_t i = 0; i < coefs.size(); ++i) {
@@ -14,16 +23,32 @@ Poly::Poly(std::vector<int64_t> coefs) {
 }
 
 Poly::Poly(std::initializer_list<std::pair<const int64_t, int64_t>> coefs) : coefs_(coefs) {
+    DeleteZeros(*this);
 }
 
 bool Poly::operator==(const Poly& other) const {
     return coefs_ == other.coefs_;
 }
 
+int64_t Poly::MyPow(int64_t base, int64_t exp) const {
+    int64_t result = 1;
+    for (;;) {
+        if (exp & 1) {
+            result *= base;
+        }
+        exp >>= 1;
+        if (!exp) {
+            break;
+        }
+        base *= base;
+    }
+    return result;
+}
+
 int64_t Poly::operator()(int64_t a) const {
     int64_t sum = 0;
     for (const auto& i : coefs_) {
-        sum += i.second * static_cast<int64_t>(pow(static_cast<double>(a), static_cast<double>(i.first)));
+        sum += i.second * Poly::MyPow(a, i.first);
     }
     return sum;
 }
@@ -36,56 +61,29 @@ Poly Poly::operator-() const {
     return new_poly;
 }
 
-Poly& operator+=(Poly& lhs, const Poly& rhs) {
+Poly& Poly::operator+=(const Poly& rhs) {
     for (const auto& i : rhs.coefs_) {
-        lhs.coefs_[i.first] += i.second;
+        coefs_[i.first] += i.second;
     }
-    std::vector<int64_t> deletes;
-    for (const auto& i : lhs.coefs_) {
-        if (!i.second) {
-            deletes.push_back(i.first);
-        }
-    }
-    for (auto i : deletes) {
-        lhs.coefs_.erase(i);
-    }
-    return lhs;
+    return Poly::DeleteZeros(*this);
 }
 
-Poly& operator-=(Poly& lhs, const Poly& rhs) {
+Poly& Poly::operator-=(const Poly& rhs) {
     for (const auto& i : rhs.coefs_) {
-        lhs.coefs_[i.first] -= i.second;
+        coefs_[i.first] -= i.second;
     }
-    std::vector<int64_t> deletes;
-    for (const auto& i : lhs.coefs_) {
-        if (!i.second) {
-            deletes.push_back(i.first);
-        }
-    }
-    for (auto i : deletes) {
-        lhs.coefs_.erase(i);
-    }
-    return lhs;
+    return Poly::DeleteZeros(*this);
 }
 
-Poly& operator*=(Poly& lhs, const Poly& rhs) {
+Poly& Poly::operator*=(const Poly& rhs) {
     Poly new_poly;
-    for (std::map<int64_t, int64_t, Cmp>::const_iterator l = lhs.coefs_.begin(); l != lhs.coefs_.end(); ++l) {
-        for (std::map<int64_t, int64_t, Cmp>::const_iterator r = rhs.coefs_.begin(); r != rhs.coefs_.end(); ++r) {
+    for (std::map<int64_t, int64_t, Poly::Cmp>::const_iterator l = coefs_.begin(); l != coefs_.end(); ++l) {
+        for (std::map<int64_t, int64_t, Poly::Cmp>::const_iterator r = rhs.coefs_.begin(); r != rhs.coefs_.end(); ++r) {
             new_poly.coefs_[l->first + r->first] += l->second * r->second;
         }
     }
-    std::vector<int64_t> deletes;
-    for (const auto& i : new_poly.coefs_) {
-        if (!i.second) {
-            deletes.push_back(i.first);
-        }
-    }
-    for (auto i : deletes) {
-        new_poly.coefs_.erase(i);
-    }
-    lhs = new_poly;
-    return lhs;
+    *this = new_poly;
+    return Poly::DeleteZeros(*this);
 }
 
 Poly operator-(const Poly& lhs, const Poly& rhs) {
@@ -108,33 +106,27 @@ Poly operator*(const Poly& lhs, const Poly& rhs) {
 
 std::ostream& operator<<(std::ostream& stream, const Poly& poly) {
     if (poly.coefs_.empty()) {
-        std::string ans = "y = 0";
-        stream << ans;
-        return stream;
+        return stream << "y = 0";
     }
-    std::string ans = "y = ";
-    std::string s;
-    size_t f = 0;
+    stream << "y = ";
+    bool first_coef = false;
     for (const auto& i : poly.coefs_) {
         auto coef = i.second;
         auto deg = i.first;
-        if (f) {
+        if (first_coef) {
             if (coef < 0) {
-                s = " - ";
+                stream << " - ";
                 coef *= -1;
             } else {
-                s = " + ";
+                stream << " + ";
             }
         }
-        f = 1;
-        ans += s;
-        ans += std::to_string(coef);
+        first_coef = true;
+        stream << coef;
         if (!deg) {
             continue;
         }
-        ans += "x^";
-        ans += std::to_string(deg);
+        stream << "x^" << deg;
     }
-    stream << ans;
     return stream;
 }
--- tasks/poly/poly.h
+++ tasks/poly/poly.h
@@ -4,28 +4,29 @@
 #include <vector>
 #include <map>
 
-struct Cmp {
-    bool operator()(int64_t a, int64_t b) const {
-        return a > b;
-    }
-};
-
 class Poly {
 public:
-    Poly();
+    Poly() = default;
     explicit Poly(std::vector<int64_t> coefs);
     Poly(std::initializer_list<std::pair<const int64_t, int64_t>> coefs);
     bool operator==(const Poly& other) const;
     Poly operator-() const;
     int64_t operator()(int64_t a) const;
     friend std::ostream& operator<<(std::ostream& stream, const Poly& poly);
-    friend Poly& operator+=(Poly& lhs, const Poly& rhs);
-    friend Poly& operator-=(Poly& lhs, const Poly& rhs);
-    friend Poly& operator*=(Poly& lhs, const Poly& rhs);
+    Poly& operator+=(const Poly& rhs);
+    Poly& operator-=(const Poly& rhs);
+    Poly& operator*=(const Poly& rhs);
     friend Poly operator-(const Poly& lhs, const Poly& rhs);
     friend Poly operator+(const Poly& lhs, const Poly& rhs);
     friend Poly operator*(const Poly& lhs, const Poly& rhs);
 
 private:
+    struct Cmp {
+        bool operator()(int64_t a, int64_t b) const {
+            return a > b;
+        }
+    };
+    int64_t MyPow(int64_t base, int64_t exp) const;
+    static Poly& DeleteZeros(Poly& p);
     std::map<int64_t, int64_t, Cmp> coefs_;
 };
