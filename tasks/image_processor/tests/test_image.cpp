f5ec31a034b0a038b1194bd0495a0368d90a11a7
Sarvar <sarvar010104@gmail.com>
2023-03-24 18:38:41 +0300

    first unit_tests

--- tasks/image_processor/tests/test_image.cpp
+++ tasks/image_processor/tests/test_image.cpp
@@ -1,4 +1,7 @@
 #include <catch.hpp>
+#include "imagework.h"
+#include "filtersmanager.h"
+#include "parser.h"
 #include "../filters/crop_filter.h"
 #include "../filters/negative_filter.h"
 #include "../filters/grayscale_filter.h"
@@ -6,16 +9,23 @@
 #include "../filters/edge_detection_filter.h"
 #include "../filters/gaussian_blur_filter.h"
 #include "../filters/emboss_filter.h"
+#include "../filters/crystallize.h"
 
-TEST_CASE("TestNegFilter") {
-    Image i = Image(
-        "/Users/mahmudovseravgan/Documents/pmi-2211-2-Sarvar-Yuldashev-notSarvar/tasks/image_processor/test_script/"
-        "data/flag.bmp");
-    Image target = Image(
-        "/Users/mahmudovseravgan/Documents/pmi-2211-2-Sarvar-Yuldashev-notSarvar/tasks/image_processor/test_script/"
-        "data/flag_neg.bmp");
+TEST_CASE("SwapEndian") {
+    unsigned int a = 0xAABBCCDD;
+    auto b = SwapEndian(a);
 
-    NegativeFilter filter = NegativeFilter();
-    filter.Apply(i);
-    REQUIRE(i.pixels == target.pixels);
+    REQUIRE(b == 0xDDCCBBAA);
+    b = SwapEndian(b);
+
+    REQUIRE(b == a);
+}
+
+TEST_CASE("ImageWorkCheck") {
+    std::string p = "/Users/mahmudovseravgan/Downloads/image_processor-main 3/tests/test_pictures/example.bmp";
+    auto i1 = Image(p);
+    REQUIRE(i1.bmp_info_header.height == 12);
+    i1.WriteBMP("/Users/mahmudovseravgan/Documents/pmi-2211-2-Sarvar-Yuldashev-notSarvar/tasks/image_processor/test_script/data/outt_flag.bmp");
+    auto i2 = Image("/Users/mahmudovseravgan/Documents/pmi-2211-2-Sarvar-Yuldashev-notSarvar/tasks/image_processor/test_script/data/outt_flag.bmp");
+    REQUIRE(i2.bmp_info_header.colors_important == i1.bmp_info_header.colors_important);
 }
