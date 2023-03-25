ba33494cb98c7de9c460267a18d8fa427de68124
Sarvar <sarvar010104@gmail.com>
2023-03-25 16:49:10 +0300

    fixing

--- tasks/image_processor/tests/test_image.cpp
+++ tasks/image_processor/tests/test_image.cpp
@@ -11,8 +11,10 @@
 #include "../filters/emboss_filter.h"
 #include "../filters/crystallize.h"
 
+const unsigned int SOME_INT = 0xAABBCCDD;
+
 TEST_CASE("SwapEndian") {
-    unsigned int a = 0xAABBCCDD;
+    unsigned int a = SOME_INT;
     auto b = SwapEndian(a);
 
     REQUIRE(b == 0xDDCCBBAA);
@@ -22,10 +24,10 @@ TEST_CASE("SwapEndian") {
 }
 
 TEST_CASE("ImageWorkCheck") {
-    std::string p = "/Users/mahmudovseravgan/Downloads/image_processor-main 3/tests/test_pictures/example.bmp";
+    std::string p = "./tests/test_pictures/example.bmp";
     auto i1 = Image(p);
     REQUIRE(i1.bmp_info_header.height == 12);
-    i1.WriteBMP("/Users/mahmudovseravgan/Documents/pmi-2211-2-Sarvar-Yuldashev-notSarvar/tasks/image_processor/test_script/data/outt_flag.bmp");
-    auto i2 = Image("/Users/mahmudovseravgan/Documents/pmi-2211-2-Sarvar-Yuldashev-notSarvar/tasks/image_processor/test_script/data/outt_flag.bmp");
+    i1.WriteBMP("./test_script/data/outt_flag.bmp");
+    auto i2 = Image("./test_script/data/outt_flag.bmp");
     REQUIRE(i2.bmp_info_header.colors_important == i1.bmp_info_header.colors_important);
 }
